eac3dc79c207fb46ec740d15e7c52342f7d419ec
Thomas Rond <AtlanteanScion@gmail.com>
2016-01-18 02:55:13 -0800

    Clean up GGH
    - Clean up and reorganize GGH code.
    - Create GGH class.
    - Remove unused code.
    - Use ZZ variable types where appropriate.
    - Add LLL-reduction to public key generation.

--- ggh.cpp
+++ ggh.cpp
@@ -11,225 +11,215 @@
 using namespace std;
 using namespace NTL;
 
-long genParam_l;
-RR genParam_k;
-long sigma;
 
-//L*U : Lower/Upper triangular, {-1,1} on diag, {-1,0,1} in triangle
-void getUnimodular(Mat<RR>& uni, const long dim_n)
+class GGH
 {
-	Mat<RR> upper = ident_mat_RR(dim_n);
-	Mat<RR> lower = ident_mat_RR(dim_n);
-	for(long i = 0; i < dim_n; ++i)
+private:
+	long dimension_n;	// Lattice dimension n
+	long sigma;	// Error vector generation parameter sigma
+	long parameter_l;	// Key generation parameter l
+	long parameter_k;	// Key generation parameter k
+	Mat<ZZ> private_R;	// Private basis R (low dual-orthogonality-defect)
+	Mat<ZZ> public_B;	// Public basis B (high dual-orthogonality-defect)
+	Mat<RR> private_R_Inv;	// R Inverse
+	Mat<RR> public_B_Inv;	// B Inverse
+	Mat<RR> transform_T;	// Private transform T
+
+public:
+	GGH(long dimension_n) : dimension_n(dimension_n)
 	{
-		for(long j = 0; j < dim_n; ++j)
+		sigma = 3;
+		parameter_l = 4;
+		parameter_k = parameter_l * ceil(1 + sqrt(dimension_n));
+
+		cout << "Generating private key...\n";
+		GeneratePrivate();
+
+		cout << "Generating public key...\n";
+		GeneratePublic();
+
+		cout << "Generating private inverse...\n";
+		GeneratePrivateInverse();
+
+		cout << "Generating public inverse...\n";
+		GeneratePublicInverse();
+
+		cout << "Generating private transform...\n";
+		GeneratePrivateTransform();
+	}
+
+	void Encrypt(Vec<ZZ>& cyphertext_c, Vec<ZZ>& plaintext_v)
+	{
+		// Error vector
+		Vec<ZZ> error_e;
+		GenerateErrorVector(error_e);
+
+		// Encrypt
+		// c = B * v + e
+		// c = R * U * v + e
+		cout << "Encrypting...\n";
+		mul(cyphertext_c, public_B, plaintext_v);
+		add(cyphertext_c, cyphertext_c, error_e);
+	}
+
+	void Decrypt(Vec<ZZ>& plaintext_v, Vec<ZZ>& cyphertext_c)
+	{
+		// Decrypt
+		// T = B^-1 * R
+		// v = T * round(R^-1 * c)
+
+		// v = B^-1 * R * round(R^-1 * (R * U * v + e))
+		// v = B^-1 * R * round(U * v + R^-1 * e)
+		// v = B^-1 * R * U * v
+		// v = B^-1 * B * v
+		// v = v
+		cout << "Decrypting...\n";
+		Vec<RR> temp;
+		mul(temp, private_R_Inv, conv<Vec<RR>>(cyphertext_c));
+		RoundVector(temp, temp);
+		mul(temp, transform_T, temp);
+		RoundVector(plaintext_v, temp);
+	}
+
+	void GenerateRandomPlaintext(Vec<ZZ>& plaintext_v)
+	{
+		stringstream ss;
+		long rand;
+		ss << "[";
+		for(int i = 0; i < dimension_n; ++i)
 		{
-			if(i > j)	// upper
+			rand = RandomBnd(2 * dimension_n + 1) - dimension_n;
+			ss << rand << " ";
+		}
+		ss << "0]";
+		ss >> plaintext_v;
+		plaintext_v.SetLength(dimension_n);	// Pad or truncate as necessary.
+	}
+
+	long GetDimension()
+	{
+		return dimension_n;
+	}
+
+private:
+	// Fill with random integers (-l,l).
+	// Add k * I.
+	void GeneratePrivate()
+	{
+		diag(private_R, dimension_n, conv<ZZ>(parameter_k));
+		for(long i = 0; i < dimension_n; ++i)
+		{
+			for(long j = 0; j < dimension_n; ++j)
 			{
-				upper[j][i] = RandomBnd(3) - 1;	// {-1,0,1}
-				lower[j][i] = 0;
-			}
-			else if(i < j)	// lower
-			{
-				upper[j][i] = 0;
-				lower[j][i] = RandomBnd(3) - 1;	// {-1,0,1}
-			}
-			else if(i == j)	// diag
-			{
-				upper[j][i] = RandomBnd(2) * 2 - 1;	// {-1,1}
-				lower[j][i] = RandomBnd(2) * 2 - 1;	// {-1,1}
+				private_R[j][i] += RandomBnd(2 * parameter_l + 1) - parameter_l;
 			}
 		}
 	}
-	mul(uni, lower, upper);
-}
 
-//fill with random integers (-l,l)
-void generatePrivateRandom(Mat<RR>& priv, const long dim_n)
-{
-	priv = ident_mat_RR(dim_n);
-	for(long i = 0; i < dim_n; ++i)
+	// Add/subtract random vectors to each vector.  Do >=2 passes.
+	void GeneratePublic()
 	{
-		for(long j = 0; j < dim_n; ++j)
+		long rand;
+		transpose(public_B, private_R);
+		for(long pass = 0; pass < 2; ++pass)
 		{
-			priv[j][i] = RandomBnd(2 * genParam_l + 1) - genParam_l;
-		}
-	}
-}
-
-//fill with random integers (-l,l)
-//add k * I
-void generatePrivateRectangular(Mat<RR>& priv, const long dim_n)
-{
-	priv = diag(dim_n, genParam_k);
-	for(long i = 0; i < dim_n; ++i)
-	{
-		for(long j = 0; j < dim_n; ++j)
-		{
-			priv[j][i] += RandomBnd(2 * genParam_l + 1) - genParam_l;
-		}
-	}
-}
-
-// Add/subtract random vectors to each vector.  Do >=2 passes.
-void generatePublicMixing(Mat<RR>& pub, const Mat<RR>& priv, const long dim_n)
-{
-	long rand;
-	pub = priv;
-	for(long pass = 0; pass < 2; ++pass)
-	{
-		for(long i = 0; i < dim_n; ++i)
-		{
-			for(long mix = 0; mix < dim_n; ++mix)
+			for(long i = 0; i < dimension_n; ++i)
 			{
-				if(i == mix)
+				for(long mix = 0; mix < dimension_n; ++mix)
 				{
-					continue;
-				}
-				
-				rand = RandomBnd(7);
-				if(rand == 0)
-				{
-					for(long j = 0; j < dim_n; ++j)
+					if(i == mix)
 					{
-						pub[j][i] += pub[j][mix];
+						continue;
+					}
+
+					rand = RandomBnd(7);
+					if(rand == 0)
+					{
+						public_B[i] += public_B[mix];
+					}
+					else if(rand == 1)
+					{
+						public_B[i] -= public_B[mix];
 					}
 				}
-				else if(rand == 1)
-				{
-					for(long j = 0; j < dim_n; ++j)
-					{
-						pub[j][i] -= pub[j][mix];
-					}
-				}
 			}
 		}
-	}
-}
 
-// Multiply private key by at least 4 random unimodular matrices.
-void generatePublicTransforms(Mat<RR>& pub, const Mat<RR>& priv, const long dim_n)
-{
-	Mat<RR> transform;
-	pub = priv;
-	for(long i = 0; i < 4; ++i)
+		// LLL-reduction
+//		ZZ det2;
+//		LLL(det2, public_B);	// exact-aritmetic (slow but sure)
+//		LLL_FP(public_B);	// floating-point (fast but only approximate)
+		G_LLL_FP(public_B);	// floating-point Givens rotations (somewhat slower than LLL_FP, but more stable)
+
+		transpose(public_B, public_B);
+	}
+
+	void GeneratePrivateInverse()
 	{
-		getUnimodular(transform, dim_n);
-		mul(pub, pub, transform);
+		inv(private_R_Inv, conv<Mat<RR>>(private_R));
 	}
-}
 
-void errVec(Vec<RR>& v, const long dim_n)
-{
-	RR r;
-	v.SetLength(dim_n);
-
-	for(long i = 0; i < dim_n; ++i)
+	void GeneratePublicInverse()
 	{
-		v[i] = sigma * (RandomBnd(2) * 2 - 1);	// {-sigma,sigma}
+		inv(public_B_Inv, conv<Mat<RR>>(public_B));
 	}
-}
 
-void roundVec(Vec<RR>& v, const long dim_n)
-{
-	for(long i = 0; i < dim_n; ++i)
-	{
-		round(v[i], v[i]);
-	}
-}
-
-int runTest()
-{
-	Mat<RR> priv_R;	// Private key (low dual-orthogonality-defect)
-	Mat<RR> pub_B;	// Public key (high dual-orthogonality-defect)
-	
-	const long dim_n = 17;
-	genParam_l = 4;
-	genParam_k = genParam_l * ceil(1 + sqrt(dim_n));
-	sigma = 3;
-	
-	cout << "Generating private key...\n";
-//	generatePrivateRandom(priv_R, dim_n);	// BUG 0% success rate
-	generatePrivateRectangular(priv_R, dim_n);	// Preferred by GGH
-//	cout << "priv_R=\n" << priv_R << "\n";
-
-	cout << "Generating public key...\n";
-	generatePublicMixing(pub_B, priv_R, dim_n);	// Preferred by GGH
-//	generatePublicTransforms(pub_B, priv_R, dim_n);
-//	cout << "pub_B=\n" << pub_B << "\n";
-
-	cout << "Generating private inverse...\n";
-	Mat<RR> priv_R_Inv;
-	inv(priv_R_Inv, priv_R);
-
-	cout << "Generating public inverse...\n";
-	Mat<RR> pub_B_Inv;
-	inv(pub_B_Inv, pub_B);
-
-//	cout << "priv_R_Inv=\n" << priv_R_Inv << "\n";
-//	cout << "pub_B_Inv=\n" << pub_B_Inv << "\n";
-
-	// Error vector
-	Vec<RR> err_e;
-	errVec(err_e, dim_n);
-//	cout << "err_e=" << err_e << "\n";
-
-	// Get message
-	Vec<RR> msg_v;
-//	cin >> msg_v;
-	stringstream ss;
-	long rand;
-	ss << "[";
-	for(int i = 0; i < dim_n; ++i)
-	{
-		rand = RandomBnd(2 * dim_n + 1) - dim_n;
-		ss << rand << " ";
-	}
-	ss << "0]";
-	ss >> msg_v;
-	msg_v.SetLength(dim_n);	// Pad or truncate as necessary.
-//	cout << "msg_v=" << msg_v << "\n";
-
-	// Encrypt
-	// c = B * v + e
-	// c = R * U * v + e
-	cout << "Encrypting...\n";
-	Vec<RR> crypt_c;
-	mul(crypt_c, pub_B, msg_v);
-	add(crypt_c, crypt_c, err_e);
-//	cout << "crypt_c=" << crypt_c << "\n";
-
-	// Decrypt
 	// T = B^-1 * R
-	// v = T * round(R^-1 * c)
-
-	// v = B^-1 * R * round(R^-1 * (R * U * v + e))
-	// v = B^-1 * R * round(U * v + R^-1 * e)
-	// v = B^-1 * R * U * v
-	// v = B^-1 * B * v
-	// v = v
-	cout << "Decrypting...\n";
-	Mat<RR> trans_T;
-	Vec<RR> dec_v;
-	mul(trans_T, pub_B_Inv, priv_R);
-	mul(dec_v, priv_R_Inv, crypt_c);
-	roundVec(dec_v, dim_n);
-	mul(dec_v, trans_T, dec_v);
-	roundVec(dec_v, dim_n);
-//	cout << "dec_v=" << dec_v << "\n";
-
-	Vec<RR> diff;
-	diff = dec_v - msg_v;
-	roundVec(diff, dim_n);
-//	cout << "diff=" << diff << "\n";
-	if(IsZero(diff))
+	void GeneratePrivateTransform()
 	{
-		cout << "\n\nSuccess\n";
+		mul(transform_T, public_B_Inv, conv<Mat<RR>>(private_R));
+	}
+
+	void GenerateErrorVector(Vec<ZZ>& v)
+	{
+		v.SetLength(dimension_n);
+
+		for(long i = 0; i < dimension_n; ++i)
+		{
+			v[i] = sigma * (RandomBnd(2) * 2 - 1);	// {-sigma,sigma}
+		}
+	}
+
+	void RoundVector(Vec<ZZ>& vec_rounded, Vec<RR>& vec)
+	{
+		vec_rounded.SetLength(vec.length());
+		for(long i = 0; i < vec.length(); ++i)
+		{
+			RoundToZZ(vec_rounded[i], vec[i]);
+		}
+	}
+
+	void RoundVector(Vec<RR>& vec_rounded, Vec<RR>& vec)
+	{
+		vec_rounded.SetLength(vec.length());
+		for(long i = 0; i < vec.length(); ++i)
+		{
+			round(vec_rounded[i], vec[i]);
+		}
+	}
+};
+
+int RunTest(GGH& ggh)
+{
+	Vec<ZZ> plaintext_v;
+	ggh.GenerateRandomPlaintext(plaintext_v);
+
+	Vec<ZZ> cyphertext_c;
+	ggh.Encrypt(cyphertext_c, plaintext_v);
+
+	Vec<ZZ> decrypted_v;
+	ggh.Decrypt(decrypted_v, cyphertext_c);
+
+	if(plaintext_v == decrypted_v)
+	{
+		cout << "\nSuccess\n";
 		return 1;
 	}
 	else
 	{
-		cout << "\n\nFail\n";
+		cout << "\nplaintext_v = \n" << plaintext_v << "\n";
+		cout << "\ndecrypted_v = \n" << decrypted_v << "\n";
+		cout << "\nFail\n";
 		return 0;
 	}
 
@@ -241,15 +231,18 @@ int main()
 	long pass = 0;
 	long total = 0;
 
-	RR::SetPrecision(300);
-	RR::SetOutputPrecision(300);
+	RR::SetPrecision(300);	// Default: 150 bits;  Minimum: 53 bits;  Maximum: 268435455 bits (NTL_OVFBND-1)
 
 	for(long i = 0; i < 100; ++i)
 	{
-		++total;
-		cout << "\n\nTest: " << total << "\n";
-		pass += runTest();
-		cout << "Success rate = " << ((float) pass/total) << "\n";
+		GGH ggh = GGH(80);
+		for(long j = 0; j < 100; ++j)
+		{
+			++total;
+			cout << "\n\nTest: " << total << "\n";
+			pass += RunTest(ggh);
+			cout << "Success rate = " << ((float) pass/total) << "\n";
+		}
 	}
 
 	return 0;
